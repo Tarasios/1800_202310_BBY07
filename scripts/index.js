466b9de08e572dbbce047fd8e932cec4f7992891
unko-chan <kevin@kliang.dev>
2023-03-07 14:54:15 -0800

    implement back function when zoomed

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,21 @@
-document.getElementById('lawnmower').addEventListener('click', function() {
-  console.log('click');
-  document.getElementById('background').style.transform= 'scale(3)';
-  document.getElementById('lawnmower').style.opacity = 1;
-  document.getElementById('lawnmower').scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
+let zoom = false;
+
+document.getElementById("lawnmower").addEventListener("click", function () {
+  zoom = true;
+  backButton();
+  document.getElementById("background").style.transform = "scale(3)";
+  document.getElementById("lawnmower").style.opacity = 1;
+  document
+    .getElementById("lawnmower")
+    .scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
 });
+
+const backButton = () => {
+  document.getElementById("back-button").style.display = "block";
+  document.getElementById("back-button").addEventListener("click", function () {
+    zoom = false;
+    document.getElementById("background").style.transform = "scale(1)";
+    document.getElementById("lawnmower").style.opacity = 0;
+    document.getElementById("back-button").style.display = "none";
+  });
+};
